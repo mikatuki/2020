1606ac916ce8782b8fea3d888fa7060c2baa3223
mikauki <kaseizin117@gmail.com>
2020-01-20 16:04:56 +0900

    jsondata 表示

--- C_word.js
+++ C_word.js
@@ -1,10 +1,8 @@
 $(loaded);  //loaded関数が実行
 
 function loaded(){
-  console.log(1111);
   $.getJSON('cWord.json',function(data){
-    console.log(111);
     console.log(data);
-     // $('.japanWord').append(data[0].jpan);
+     $('.japanWord').append(data[0].jpan);
   });
 }
