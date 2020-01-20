3f7310b68d7dff475435bf036d57f981b46355fa
mikauki <kaseizin117@gmail.com>
2020-01-20 16:06:38 +0900

    綴りミス

--- C_word.js
+++ C_word.js
@@ -3,6 +3,6 @@ $(loaded);  //loaded関数が実行
 function loaded(){
   $.getJSON('cWord.json',function(data){
     console.log(data);
-     $('.japanWord').append(data[0].jpan);
+     $('.japanWord').append(data[0].japan);
   });
 }
